1c90744e71c978289b17e64618c5d642a6aec6d3
observo <observo.code@gmail.com>
2019-04-26 05:05:43 +0600

    Already

--- UploadFinal.sh
+++ UploadFinal.sh
@@ -7,28 +7,29 @@
 #======================================
 #CALL FORMAT: ./UploadFinal.sh FileName
 #======================================
-FileName=$1
+FullFileName=$1
 BaseURL=http://rapidgator.net/api/v2/
 
-Hash=$(md5sum "$FileName" | cut -d ' ' -f 1)
-#echo $Hash
-FileSize=$(stat -c%s "$FileName")
-#echo $FileSize
-FileName=$(basename $FileName)
-#echo $FileName
+Hash=$(md5sum "$FullFileName" | cut -d ' ' -f 1)
+echo $Hash
+FileSize=$(stat -c%s "$FullFileName")
+echo $FileSize
+FileName=$(basename $FullFileName)
+echo $FileName
 #LOGIN REQUEST
 LoginResponse=$(curl "$BaseURL"'user/login?login=YOUR_LOGIN&password=YOUR_PASSWORD'|grep -E 'response')
 #echo $LoginResponse
 Status=$(echo $LoginResponse| grep -m1  -oP '\s*"status"\s*:\s*\K[^,]+')
-#echo $Status
+echo $Status
 Token=$(echo $LoginResponse| grep -m1 -oP '\s*"token"\s*:\s*"\K[^"]+')
-#echo $Token
+echo $Token
 
 #if $Status -eq 200 then
 	#UPLOAD REQUEST
 	
 	echo "$BaseURL"'file/upload?token='"$Token"'&name='"$FileName"'&hash='"$Hash"'&size='"$FileSize"
-	FileUploadResponse=$(curl -H "CURLOPT_HEADER:false" -H "CURLOPT_RETURNTRANSFER:true" "$BaseURL"'file/upload?token='"$Token"'&hash='"$Hash"'&size='"$FileSize"'&name='"$FileName"|grep -E 'response')
+	#FileUploadResponse=$(curl '-header CURLOPT_HEADER:false -header CURLOPT_RETURNTRANSFER:true '"$BaseURL"'file/upload?token='"$Token"'&hash='"$Hash"'&size='"$FileSize"'&name='"$FileName"|grep -E 'response')
+	FileUploadResponse=$(curl "$BaseURL"'file/upload?token='"$Token"'&hash='"$Hash"'&size='"$FileSize"'&name='"$FileName"|grep -E 'response')
 	echo $FileUploadResponse
 	UploadID=$(echo $FileUploadResponse| grep -m1 -oP '\s*"upload_id"\s*:\s*"\K[^"]+')
 	echo $UploadID
@@ -42,6 +43,18 @@ Token=$(echo $LoginResponse| grep -m1 -oP '\s*"token"\s*:\s*"\K[^"]+')
 	echo $Status
 	Details=$(echo $FileUploadResponse| grep -m1 -oP '\s*"details"\s*:\s*"\K[^,}]+')
 	echo $Details
+	
+	#FileResponse=$(echo '<form method="post" action='"$URL"' enctype="multipart/form-data">''<input type="file" name='"$FullFileName"'/>''</form>')
+	
+	#@RGBash.txt http:\/\/pr31.rapidgator.net\/upload\/api?uuid=rU4ZHNFWnesIJ9s0c3fp4vzteh44g4kc&sid=ce896f03943165a65f5599f7663ec148
+	echo '-X POST -d ''@'"$FullFileName"' '"$URL"
+	FileResponse=$(curl '-X POST -d ''@'"$FullFileName"' '"$URL")
+	echo $FileResponse	
+	#Warning: Couldn't read data from file "RGBash.txt
+	#this makes an empty POST
+	#curl: no URL specified!
+
+
 	echo "$BaseURL"'file/upload_info?token='"$Token"'&upload_id='"$UploadID";
 	FileUploadInfoResponse=$(curl "$BaseURL"'file/upload_info?token='"$Token"'&upload_id='"$UploadID"|grep -E 'response')
 	echo $FileUploadInfoResponse
