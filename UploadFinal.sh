b4e7bb35212c4625a6805ac42a995ce2dec52ee4
observo <observo.code@gmail.com>
2019-04-26 02:29:58 +0600

    Almost

--- UploadFinal.sh
+++ UploadFinal.sh
@@ -1,21 +1,34 @@
+#=====================================
+#PHP EQUIVALENT
+#curl_setopt($this->_ch, CURLINFO_HEADER_OUT, true);
+#$result = curl_exec($this->_ch);
+#$response = curl_getinfo($this->_ch, CURLINFO_HEADER_OUT);
+#======================================
 #======================================
 #CALL FORMAT: ./UploadFinal.sh FileName
 #======================================
 FileName=$1
+BaseURL=http://rapidgator.net/api/v2/
+
 Hash=$(md5sum "$FileName" | cut -d ' ' -f 1)
-echo $Hash
-FileSize=$(stat -c %s "$FileName")
-echo $FileSize
+#echo $Hash
+FileSize=$(stat -c%s "$FileName")
+#echo $FileSize
+FileName=$(basename $FileName)
+#echo $FileName
 #LOGIN REQUEST
-#LoginResponse=$(curl 'https://rapidgator.net/api/v2/user/login?login=YOUR_LOGIN&password=YOUR_PASSWORD'|grep -E 'response')
-echo $LoginResponse
-Token=$(echo $LoginResponse| grep -m1 -oP '\s*"token"\s*:\s*"\K[^"]+')
-echo $Token
+LoginResponse=$(curl "$BaseURL"'user/login?login=YOUR_LOGIN&password=YOUR_PASSWORD'|grep -E 'response')
+#echo $LoginResponse
 Status=$(echo $LoginResponse| grep -m1  -oP '\s*"status"\s*:\s*\K[^,]+')
-echo $Status
-if [$Status==200]; then
+#echo $Status
+Token=$(echo $LoginResponse| grep -m1 -oP '\s*"token"\s*:\s*"\K[^"]+')
+#echo $Token
+
+#if $Status -eq 200 then
 	#UPLOAD REQUEST
-	FileUploadResponse=$(curl 'https://rapidgator.net/api/v2/file/upload?token='"$Token"'&hash='"$Hash"'&size='"$FileSize"'&name='"$FileName"|grep -E 'response')
+	
+	echo "$BaseURL"'file/upload?token='"$Token"'&name='"$FileName"'&hash='"$Hash"'&size='"$FileSize"
+	FileUploadResponse=$(curl -H "CURLOPT_HEADER:false" -H "CURLOPT_RETURNTRANSFER:true" "$BaseURL"'file/upload?token='"$Token"'&hash='"$Hash"'&size='"$FileSize"'&name='"$FileName"|grep -E 'response')
 	echo $FileUploadResponse
 	UploadID=$(echo $FileUploadResponse| grep -m1 -oP '\s*"upload_id"\s*:\s*"\K[^"]+')
 	echo $UploadID
@@ -29,5 +42,6 @@ if [$Status==200]; then
 	echo $Status
 	Details=$(echo $FileUploadResponse| grep -m1 -oP '\s*"details"\s*:\s*"\K[^,}]+')
 	echo $Details
-	FileUploadInfoResponse=$(curl 'https://rapidgator.net/api/v2/file/upload_info?token='"$Token"'&upload_id='"$UploadID"|grep -E 'response')
+	echo "$BaseURL"'file/upload_info?token='"$Token"'&upload_id='"$UploadID";
+	FileUploadInfoResponse=$(curl "$BaseURL"'file/upload_info?token='"$Token"'&upload_id='"$UploadID"|grep -E 'response')
 	echo $FileUploadInfoResponse
